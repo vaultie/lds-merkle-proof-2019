70d0f097753de564cdc913cbbb89eea9ece80fb7
Dmitry Semenovsky <dsemenovsky@gmail.com>
2019-10-21 17:00:07 -0400

    Export Encoder and Decoder from src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
-const cbor = require('cbor')
-const { Map } = require('cbor')
-const multibase = require('multibase')
-const _ = require('lodash')
-let json = require('./test.json')
+const Encoder = require('./Encoder')
+const Decoder = require('./Decoder')
 
-json.merkleRoot = cbor.encode(Buffer.from(json.merkleRoot, 'hex'))
-json.targetHash = cbor.encode(Buffer.from(json.targetHash, 'hex'))
-
-json.path = _.flatten(json.path.map(item => {
-  return Object.keys(item).map(key => {
-    return new Map([[0, Buffer.from(item[key], 'hex')]])
-  })
-}))
-
-json.anchors = json.anchors.map(anchor => {
-  return new Map([[0, Buffer.from(anchor.sourceId, 'hex')], [1, anchor.type]])
-})
-
-const jsonCbor = cbor.encode(json)
-console.log(jsonCbor.toString('hex'))
-const jsonCborBase58 = multibase.encode('base58btc', jsonCbor)
-
-console.log(jsonCborBase58.toString())
-console.log(jsonCborBase58.toString().length)
+module.exports = {
+  Encoder,
+  Decoder
+}
