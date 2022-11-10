78cb16d689857839275f394c853bee9543edff62
Piotr Nalewajka <piotr.nalewajka@hyland.com>
2022-11-10 14:47:46 +0100

    Add tests for encoding and decoding mocknet values

--- src/__tests__/Decoder.test.js
+++ src/__tests__/Decoder.test.js
@@ -1,7 +1,9 @@
 const Decoder = require('../Decoder')
 const proofValue = require('./proofValue.json')
+const mocknetProofValue = require('./mocknetProofValue.json')
 
 const resultBase58 = 'z6nGv6rMRybRe9CuMzbQbdu7sA858v1d13JU3hoAr1x93cheinB35kDXqCvaA93WTLWGtLZMdQSvvNCxEMZPhLvDa4CbUYkm4pCwBe7kCZAsuwHZwHxgyzCbRUWFbMXHhkVSHoPYmPzfi4arfHKMgKSurZ7oqe3GHRdi78TbHGvA65edK8JBEdTUt8SpCdc7wz5qiwj3THtcNAXfgK4LmCAu4fq8CnjLcMtGoEdfXfjy3turtaTapyM3katuYKAzbJF3FiE8i8NXBsiBnEbvKk7k'
+const mocknetResultBase58 = 'zYaPdLQCuei4vhe5Bdz4GjXD4iYkEZL1x2jgNjJArVBt29RUn14RZspAqFo2XDwKtTdBCRZtzxpTLwGhJxKv7TGeQQpBbgLgtM6vF15cBc4TBwd3sJc2ZxEjve6Ncuu1XMwczshC8injjzUpjLhhwKTijnUSdoxwj5jyv9CztRmkDajRZAQmaqgXuCG8BtoLqsbCubwbpbvEHPJrKjqHrt99C6zPx5X89C8eNMFFr5d7GSSWBbAHrwoCbjYvepm'
 
 describe('Decoder', () => {
   test('should create instance from valid props', () => {
@@ -24,5 +26,12 @@ describe('Decoder', () => {
     const result = decoder.decode()
     
     expect(result).toEqual(proofValue)
+  })
+
+  test('should decode a mock proofValue', () => {
+    const decoder = new Decoder(mocknetResultBase58)
+    const result = decoder.decode()
+
+    expect(result).toEqual(mocknetProofValue)
   })
 })
--- src/__tests__/Encoder.test.js
+++ src/__tests__/Encoder.test.js
@@ -1,7 +1,9 @@
 const Encoder = require('../Encoder')
 const proofValue = require('./proofValue.json')
+const mocknetProofValue = require('./mocknetProofValue.json')
 
 const resultBase58 = 'z6nGv6rMRybRe9CuMzbQbdu7sA858v1d13JU3hoAr1x93cheinB35kDXqCvaA93WTLWGtLZMdQSvvNCxEMZPhLvDa4CbUYkm4pCwBe7kCZAsuwHZwHxgyzCbRUWFbMXHhkVSHoPYmPzfi4arfHKMgKSurZ7oqe3GHRdi78TbHGvA65edK8JBEdTUt8SpCdc7wz5qiwj3THtcNAXfgK4LmCAu4fq8CnjLcMtGoEdfXfjy3turtaTapyM3katuYKAzbJF3FiE8i8NXBsiBnEbvKk7k'
+const mocknetResultBase58 = 'zYaPdLQCuei4vhe5Bdz4GjXD4iYkEZL1x2jgNjJArVBt29RUn14RZspAqFo2XDwKtTdBCRZtzxpTLwGhJxKv7TGeQQpBbgLgtM6vF15cBc4TBwd3sJc2ZxEjve6Ncuu1XMwczshC8injjzUpjLhhwKTijnUSdoxwj5jyv9CztRmkDajRZAQmaqgXuCG8BtoLqsbCubwbpbvEHPJrKjqHrt99C6zPx5X89C8eNMFFr5d7GSSWBbAHrwoCbjYvepm'
 
 describe('Encoder', () => {
   test('should create instance from valid props', () => {
@@ -24,5 +26,12 @@ describe('Encoder', () => {
     const result = encoder.encode()
 
     expect(result.toString()).toEqual(resultBase58)
+  })
+
+  test('should encode a mock proofValue', () => {
+    const encoder = new Encoder(mocknetProofValue)
+    const result = encoder.encode()
+
+    expect(result.toString()).toEqual(mocknetResultBase58)
   })
 })
